fbf0da77ef5509e16cd9b128b78b6568b6bf27e6
terrycojones <terry@fluidinfo.com>
2012-11-27 16:04:12 +0000

    Add context menu to disable, with auto-opening of all pending URLs.

--- README.md
+++ README.md
@@ -4,20 +4,39 @@ When you click on any link, or open a tab for new URL, the current tab will
 immediately be closed and the desired URL will be displayed in a new tab at
 some random point in the future.
 
-This is just a silly hack meant to illustrate alternate browsing
-possibilities.  In this case the idea is to make browsing less immediate
-and more considered.
+The extension installs a (right-click) context menu that lets you disable
+it. On disable, all pending URLs will immediately be opened in the order
+they were originally clicked.
+
+## Why?
+
+This is a silly hack to illustrate alternate browsing possibilities.  In
+this case the idea is to make browsing less immediate and more considered.
+
+## Installation from a bundled .crx file
+
+You should be able to easily install the extension by visting
+https://fluiddb.fluidinfo.com/about/open-it-later/fluidinfo.com/chrome.crx
+
+## Installation from source
+
+* Download the repo: `git clone http://github.com/terrycojones/open-it-later`
+* In chrome, go to `chrome://extensions`
+* Click on `Developer mode`
+* Click on `Load Unpacked Extension...`
+* Navigate to the directory where you cloned the repo and click `Open`
 
 ## Internals
 
-Change the minimum and maximum wait times in background.js
+Change the minimum and maximum wait times (`TC.minWait` and `TC.minWait`)
+in background.js
 
 Look at the console log for the extension background page to see when URLs
 you try to open will actually be opened.  To see the console, go to
-chrome://extensions and click the _generated_background_page.html link
+`chrome://extensions` and click the `_generated_background_page.html` link
 next to the Open-it-later extension.
 
 ## Image credit
 
-The "snake oil" icon used by the extension comes from Hugh McLeod at
-http://gapingvoid.com/
+The "snake oil" icon used by the extension comes from
+[Hugh McLeod](http://gapingvoid.com/)
--- background.js
+++ background.js
@@ -1,21 +1,29 @@
 var TC = {
+    enabled: true,
     maxWait: 300, // Maximum wait (seconds) before a URL opens.
+    menuItem: null,
     minWait: 15, // Minimum wait (seconds) for a URL to open.
+    pendingCount: 0,  // Number of items in urls object (below).
     urls: {},
-    
+
     check: function(tab){
         var url = tab.url;
-        if (url.indexOf('chrome://') === 0 || url.indexOf('chrome-devtools://') === 0){
+        if (!this.enabled ||
+            url.indexOf('chrome://') === 0 ||
+            url.indexOf('chrome-devtools://') === 0){
             // Let empty new tabs and developer tabs be opened.
             return;
         }
-        
+
         var now = Date.now();
-        
+
         if (this.urls[url]){
-            if (now - this.urls[url] < 3000){
-                // This URL was scheduled for opening, and the timing is ok.
+            if (this.urls[url].timeout === null){
+                // This URL was scheduled for opening, and its timeout has fired.
+                console.log('Automatically opening ' + url);
                 delete this.urls[url];
+                this.pendingCount--;
+                this.updateMenuItem();
             }
             else {
                 console.log('url ' + url + ' is scheduled to be opened, but not now!');
@@ -27,24 +35,85 @@ var TC = {
             var delta = 1000 * (
                 this.minWait + Math.random() * (this.maxWait - this.minWait));
             console.log(url + ' will be opened in ' + (delta / 1000) + ' seconds.');
-            this.urls[url] = now + delta;
-            setTimeout(
+            var timeout = setTimeout(
                 function(){
+                    this.urls[url].timeout = null;
                     chrome.tabs.create({
                         active: false,
                         url: url
                     });
-                },
+                }.bind(this),
                 delta);
+            this.urls[url] = {
+                scheduledAt: now,
+                timeout: timeout,
+                when: now + delta
+            };
+            this.pendingCount++;
+            this.updateMenuItem();
             chrome.tabs.remove(tab.id);
         }
     },
-    
-    init: function(){
 
+    openAllPending: function(){
+        // Open all pending URLs, in the order they were scheduled to be opened.
+        var toOpen = [];
+        for (var url in this.urls){
+            clearTimeout(this.urls[url].timeout);
+            toOpen.push(url);
+        }
+        toOpen.sort(function(a, b){
+            return this.urls[a].scheduledAt < this.urls[b].scheduledAt ? -1 : 1;
+        }.bind(this));
+        for (var i = 0; i < toOpen.length; i++){
+            chrome.tabs.create({
+                active: false,
+                url: toOpen[i]
+            });
+        }
+        this.urls = {};
+        this.pendingCount = 0;
+        this.updateMenuItem();
+    },
+
+    updateMenuItem: function(){
+        if (this.enabled){
+            chrome.contextMenus.update(
+                this.menuItem, {
+                    checked: true,
+                    title: 'Enabled, ' + this.pendingCount + ' URL' + (this.pendingCount === 1 ? '' : 's') + ' pending'
+                }
+            );
+        }
+        else {
+            chrome.contextMenus.update(
+                this.menuItem, {
+                    checked: false,
+                    title: 'Disabled, click to enable'
+                }
+            );
+        }
+    },
+
+    init: function(){
         chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
             changeInfo.status === 'loading' && this.check(tab);
         }.bind(this));
+
+        this.menuItem = chrome.contextMenus.create({
+            checked: true,
+            title: 'Enabled. 0 URLs pending.',
+            contexts: ['all'],
+            type: 'checkbox',
+            onclick : function(info, tab){
+                this.enabled = info.checked;
+                this.updateMenuItem();
+                if (!this.enabled){
+                    // Deactivated :-(
+                    this.openAllPending();
+                }
+            }.bind(this)
+        });
     }
 };
 
